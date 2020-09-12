868c1e60e2c437c4556f822bf75956623f506535
JamesMcGaa <jemcgaa@gmail.com>
2020-09-12 15:42:18 -0400

    separating stats, adding csv support

--- constants.py
+++ constants.py
@@ -1,6 +1,59 @@
-EXPANSION_RARITY_DISTRIBUTION = {
-    'ZNR': [101, 80, 64, 20]
+EXPANSION_RARITY_STATS = {
+    "ZNR": {
+        "rarity_distribution": [101, 80, 64, 20],
+        "mythic_rate": 1 / 7.4,
+    },
+    
+    "M21": {
+        "rarity_distribution": [111, 80, 53, 15],
+        "mythic_rate": 1 / 8,
+    },
+    "IKO": {
+        "rarity_distribution": [111, 80, 53, 15],
+        "mythic_rate": 1 / 8,
+    },
+    "THB": {
+        "rarity_distribution": [101, 80, 53, 15],
+        "mythic_rate": 1 / 8,
+    },
+    "ELD": {
+        "rarity_distribution": [101, 80, 53, 15],
+        "mythic_rate": 1 / 8.0,
+    },
+
+    "M20": {
+        "rarity_distribution": [112, 80, 53, 15],
+        "mythic_rate": 1 / 8,
+    },
+    "WAR": {
+        "rarity_distribution": [101, 80, 53, 15],
+        "mythic_rate": 1 / 8,
+    },
+    "RNA": {
+        "rarity_distribution": [111, 80, 53, 15],
+        "mythic_rate": 1 / 8,
+    },
+    "GRN": {
+        "rarity_distribution": [111, 80, 53, 15],
+        "mythic_rate": 1 / 8.0,
+    },
+
+    "M19": {
+        "rarity_distribution": [111, 80, 53, 16],
+        "mythic_rate": 1 / 8,
+    },
+    "DOM": {
+        "rarity_distribution": [101, 80, 53, 15],
+        "mythic_rate": 1 / 8,
+    },
+    "ROI": {
+        "rarity_distribution": [70, 60, 48, 13],
+        "mythic_rate": 1 / 8,
+    },
+    "XLN": {
+        "rarity_distribution": [101, 80, 63, 15],
+        "mythic_rate": 1 / 8.0,
+    },
 }
 
-CARDS_PER_RARITY = [5,2,1,1]
-
+CARDS_PER_RARITY = [5, 2, 1, 1]
---
+++ stats.py
@@ -0,0 +1,50 @@
+from constants import EXPANSION_RARITY_STATS
+from mtga_set_completion import mtg_set
+import pandas as pd
+import numpy as np
+
+
+def experiment(num_trials, verbose=False):
+    col_names = [
+        "Expansion Code",
+        "Packs Purchased",
+        "Packs Opened",
+        "Standard Dev",
+        "min",
+        "5%",
+        "median",
+        "95%",
+        "max",
+    ]
+
+    data = {}
+    for col_name in col_names:
+        data[col_name] = []
+
+    for expansion_code in EXPANSION_RARITY_STATS:
+        packs_purchased = []
+        packs_opened = []
+        for t in range(num_trials):
+            if verbose and t % 100 == 0:
+                print("Running trial {} out of {}".format(t, num_trials))
+            mtg_set_instance = mtg_set(EXPANSION_RARITY_STATS[expansion_code])
+            packs_purchased.append(mtg_set_instance.complete_set())
+            packs_opened.append(mtg_set_instance.packs_opened)
+
+        data["Expansion Code"].append(expansion_code)
+        data["Packs Purchased"].append(np.average(packs_purchased))
+        data["Standard Dev"].append(np.std(packs_purchased))
+        data["min"].append(np.min(packs_purchased))
+        data["5%"].append(np.percentile(packs_purchased, 5))
+        data["median"].append(np.percentile(packs_purchased, 50))
+        data["95%"].append(np.percentile(packs_purchased, 95))
+        data["max"].append(np.max(packs_purchased))
+        data["Packs Opened"].append(np.average(packs_opened))
+
+    pd.DataFrame(data, columns=col_names).to_csv(
+        "{}k.csv".format(num_trials // 1000)
+    )
+
+
+if __name__ == "__main__":
+    experiment(10000)
